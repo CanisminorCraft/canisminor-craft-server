a4742825176b884af5c42d1e485f8f551350afe3
canisminor1990 <canisminor1990@gmail.com>
2016-12-12 12:28:26 +0800

    sync

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Dlog4j.configurationFile=log4j2.xml -d64 -server -XX:+UseConcMarkSweepGC -Xmx900M -Xms512M -jar spigot-1.11.jar nogui
+java -Xmx900M -Xms512M -jar spigot-1.11.jar nogui
