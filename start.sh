acd59bcce324f6b9fe090082f09d330b9a25a7ea
canisminor1990 <canisminor1990@gmail.com>
2016-12-14 01:35:19 +0800

    sync

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-java -Xmx831M -Xms512M -XX:+AggressiveOpts -XX:+UseCompressedOops -jar 
-spigot-1.11.jar nogui
+java -Xmx831M -Xms512M -XX:+AggressiveOpts -XX:+UseCompressedOops -jar spigot-1.11.jar nogui
